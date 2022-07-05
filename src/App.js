739cad118996e11e665b339a88fa72f479e45b88
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-07-06 02:52:20 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import Header from './Header';
-import Home from './Home';
-import Checkout from './Checkout';
+import Home from "./Home";
+import Header from "./Header";
+import Checkout from "./Checkout";
+import Login from "./Login";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,11 @@ function App() {
       <Router>
 
         <Switch>
+
+          <Route path="/login">
+            <Login />
+          </Route>
+
           <Route path="/checkout">
             <Header />
             <Checkout />
@@ -19,6 +25,7 @@ function App() {
             <Header />
             <Home />
           </Route>
+
         </Switch>
 
       </Router>
