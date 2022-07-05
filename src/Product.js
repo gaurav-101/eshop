850d58e30ce9f52451db81147f52981307da7302
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-07-06 02:58:16 +0530

    init

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,22 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
 function Product({id, title, image, price, rating}) {
+
+    const [state, dispatch] = useStateValue();
+
+    const addToBasket = () => {
+        dispatch({
+            type: "ADD_TO_BASKET",
+            item: {
+                id: id,
+                image: image,
+                price: price,
+                rating: rating,
+            },
+        });
+    };
 
     return (
         <div className="product">
@@ -20,9 +35,9 @@ function Product({id, title, image, price, rating}) {
                 </div>
             </div>
 
-            <img src={image} alt=""/>
+            <img src={image} />
 
-            <button>Add to Basket</button>
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
     )
 }
