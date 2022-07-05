b41c33dc933061b8f6a42107e9a2ef5f821e3bdf
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-07-05 17:37:37 +0530

    props

--- src/Product.js
+++ src/Product.js
@@ -6,11 +6,18 @@ function Product({id, title, image, price, rating}) {
     return (
         <div className="product">
             <div className="product__info">
-                <p>Title</p>
+                <p>{title}</p>
                 <p className="product__price">
-                    <small>$30</small>
+                    <small>$</small>
+                    <strong>{price}</strong>
                 </p>
-                <div className="product__rating"> ⭐⭐⭐ </div>
+                <div className="product__rating">
+                    {Array(rating)
+                    .fill()
+                    .map((_, i) => (
+                        <p>⭐</p>
+                    ))}
+                </div>
             </div>
 
             <img src={image} />
